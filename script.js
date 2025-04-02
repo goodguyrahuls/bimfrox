9b2f27578fb5594bd76c3f9c5d7ed12065d60148
goodguyrahuls <rctecg@gmail.com>
2025-04-02 18:55:35 +0530

    styling,new data add ,  change

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ var navLinks = document.getElementById("navLinks");
         function hideMenu() {
             navLinks.style.right = "-200px";
         }
+
+        var swiper = new Swiper(".mySwiper", {
+            loop: true,  // Infinite loop
+            autoplay: {
+                delay: 2500,  // Auto slide every 2.5 seconds
+                disableOnInteraction: false,
+            },
+            effect: "fade", // Smooth transition effect
+        });
