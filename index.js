0ffa171425a2869bc5893a281ebf55f088edfb74
Mayur Borse <mayurborse9119@gmail.com>
2025-04-09 17:02:18 +0530

    added routes folder

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ const port = process.env.PORT || 3000;
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
+//routes
+const frox = require("./routes/frox.js");
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
@@ -28,59 +31,10 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected Successfully"))
   .catch(err => console.error("MongoDB Connection Error:", err));
 
+//bimfrox route
+app.use("/frox", frox)
 
 
-//Index route
-app.get("/frox", (req, res) => {
-    res.render("frox/index.ejs");
-})
-
-//Contact route
-app.get("/frox/contact", (req, res) => {
-    res.render("frox/contact.ejs");
-})
-
-app.post("/frox/contact", async(req, res) => {
-    let {name, ph, email, message} = req.body;
-    let client = new Contact({
-        name: name,
-        ph: ph,
-        email: email,
-        message: message,
-    })
-    await client.save();
-    res.redirect("/frox");
-})
-
-app.get("/frox/admin", async(req, res) => {
-    let contacts = await Contact.find();
-    res.render("frox/admin.ejs", { contacts });
-})
-
-app.get("/frox/about", (req, res) => {
-    res.render("frox/about.ejs");
-})
-
-app.get("/frox/services", (req, res) => {
-    res.render("frox/service.ejs");
-})
-
-app.get("/frox/projects", (req, res) => {
-    res.render("frox/project.ejs");
-})
-
-app.get("/frox/blogs", (req, res) => {
-    res.render("frox/blog.ejs");
-})
-
-app.get("/frox/program", (req, res) => {
-    res.render("frox/program.ejs")
-})
-
-
-app.get("/frox/students", (req, res) => {
-    res.render("")
-})
 
 app.post("/students", upload.single("resume"), async (req, res) => {
     try {
---
+++ routes/frox.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const router = express.Router();
+
+//Index route
+router.get("/", (req, res) => {
+    res.render("frox/index.ejs");
+})
+
+//Contact route
+router.get("/contact", (req, res) => {
+    res.render("frox/contact.ejs");
+})
+
+router.post("/contact", async(req, res) => {
+    let {name, ph, email, message} = req.body;
+    let client = new Contact({
+        name: name,
+        ph: ph,
+        email: email,
+        message: message,
+    })
+    await client.save();
+    res.redirect("/frox");
+})
+
+router.get("/admin", async(req, res) => {
+    let contacts = await Contact.find();
+    res.render("frox/admin.ejs", { contacts });
+})
+
+router.get("/about", (req, res) => {
+    res.render("frox/about.ejs");
+})
+
+router.get("/services", (req, res) => {
+    res.render("frox/service.ejs");
+})
+
+router.get("/projects", (req, res) => {
+    res.render("frox/project.ejs");
+})
+
+router.get("/blogs", (req, res) => {
+    res.render("frox/blog.ejs");
+})
+
+router.get("/program", (req, res) => {
+    res.render("frox/program.ejs")
+})
+
+
+router.get("/students", (req, res) => {
+    res.render("")
+})
+
+module.exports = router;
