2a64a626df1f915a95506f9efd696cdb834229fc
goodguyrahuls <rctecg@gmail.com>
2025-04-10 13:47:35 +0530

    update project  section

--- public/script.js
+++ public/script.js
@@ -7,4 +7,19 @@ var navLinks = document.getElementById("navLinks");
             navLinks.style.right = "-200px";
         }
 
-        
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('animate');
+        observer.unobserve(entry.target); // ek baar animation ho jaye toh dubara nahi hoga
+      }
+    });
+  }, {
+    threshold: 0.1
+  });
+
+  document.querySelectorAll('.fade-up').forEach(el => {
+    observer.observe(el);
+  });
+
